7aef47fceb731bd4654c49b45e931683e436f299
Prachi <prachishah2401@gmail.com>
2020-07-27 17:27:17 +1000

    Added toolbar which includes logo of the application, resuable navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-
+import ToolBar from '../Navigation/Toolbar/ToolBar';
 import classes from './Layout.module.css';
 
 const Layout = ( props ) => (
     <Aux>
-        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <ToolBar />
         <main className={classes.Content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/ToolBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './ToolBar.module.css';
+
+const ToolBar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+        <Logo />
+        <nav>
+            <NavigationItems />
+        </nav>
+    </header>
+);
+
+export default ToolBar;
