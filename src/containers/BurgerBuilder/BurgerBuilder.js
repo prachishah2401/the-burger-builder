8741eefc5852bbfa4f3aec3d977773c87ca67aa4
Prachi <prachishah2401@gmail.com>
2020-08-12 14:26:55 +1000

    Created axios instance for orders to send a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
     salad: 0.5,
@@ -88,7 +89,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue');
+       // alert('You continue');
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer: {
+               name: 'Prachi Shah',
+               address: {
+                   street: 'Church St',
+                   zipCode: '2150',
+                   country: 'Australia'
+               },
+               email: 'prachi@test.com'
+           },
+           deliveryMethod: 'fastest'
+       }
+       axios.post('/orders.json', order)
+        .then(response =>console.log(response))
+        .catch(error => console.log(error));
     }
 
     render() {
